0117b6747316b2e0e08dc47a2d88518b4ab9cbe8
HenryHe613 <admin@nanyan.cc>
2024-12-11 00:23:17 +0800

    电梯有人了

--- Elevator/main.c
+++ Elevator/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 #ifdef _WIN32
 #include<windows.h>
 #endif
@@ -9,6 +10,7 @@
 #define HEIGHT 30
 #define ELEVATOR_HEIGHT 4
 #define ELEVATOR_WIDTH 12
+#define ELEVATOR_WEIGHT 1000
 #define FLOOR_HEIGHT 4
 #define FLOOR_COUNT 7
 #define FLOOR_WIDTH 8
@@ -22,6 +24,19 @@ void screenPrintString(int x, int y, char* s){
 void screenPrintNumber(int x, int y, int d){
     printf("\033[%d;%dH%d", x+1, y*2+1, d);
     fflush(stdout);
+}
+
+// Log
+
+int logCount = 0;
+void printLog(char* s){
+    char logContent[100];
+    snprintf(logContent, (WIDTH-ELEVATOR_WIDTH-FLOOR_WIDTH-1)*2, "%d: %s", logCount+1, s);
+    screenPrintString((logCount)%HEIGHT+1, ELEVATOR_WIDTH+FLOOR_WIDTH+2, logContent);
+    for(int i=ELEVATOR_WIDTH+FLOOR_WIDTH+2;i<=WIDTH;i++){
+        screenPrintString((logCount+1)%HEIGHT+1, i, "  ");
+    }
+    logCount++;
 }
 
 // Block
@@ -54,7 +69,9 @@ struct Elevator{
     int x; //position
     int direction; //1:up -1:down
     int floor;
-}elevator = {.x=1, .direction=1, .floor=FLOOR_COUNT};
+    int weight;
+    int count;
+}elevator = {.x=1, .direction=1, .floor=FLOOR_COUNT, .weight=0, .count=0};
 
 void refreshElevator(){
     for(int i=1;i<HEIGHT;i++){
@@ -76,7 +93,11 @@ void refreshElevator(){
     }else{
         screenPrintString(elevator.x+1,2,"▼");
     }
-    screenPrintString(elevator.x+ELEVATOR_HEIGHT-1,2,"LOOPY");
+    screenPrintString(elevator.x+1,7,"W:");
+    screenPrintNumber(elevator.x+1,8,elevator.weight);
+    screenPrintString(elevator.x+1,4,"C:");
+    screenPrintNumber(elevator.x+1,5,elevator.count);
+    // screenPrintString(elevator.x+ELEVATOR_HEIGHT-1,2,"LOOPY");
 }
 
 void elevatorUp(){
@@ -108,6 +129,41 @@ void elevatorGo(int floor){
     elevator.floor = floor;
 }
 
+// People
+
+typedef struct People People;
+struct People{
+    char name[100];
+    int nowFloor;
+    int targetFloor;
+    int weight;
+    int onElevator;
+}people[100];
+int peoCur = 0;
+const char *first_names[10] = {
+    "John", "Michael", "William", "David", "Richard", 
+    "Joseph", "Thomas", "Charles", "Christopher", "Daniel"
+};
+const char *last_names[10] = {
+    "Smith", "Johnson", "Brown", "Williams", "Jones",
+    "Miller", "Davis", "Garcia", "Rodriguez", "Martinez"
+};
+void generatePeople(){
+    snprintf(people[peoCur].name, 100, "%s %s", first_names[rand()%10], last_names[rand()%10]);
+    people[peoCur].nowFloor = rand()%FLOOR_COUNT+1;
+    people[peoCur].targetFloor = rand()%FLOOR_COUNT+1;
+    while(people[peoCur].targetFloor==people[peoCur].nowFloor){
+        people[peoCur].targetFloor = rand()%FLOOR_COUNT+1;
+    }
+    people[peoCur].weight = rand()%100+50;
+    people[peoCur].onElevator = 0;
+    peoCur++;
+    // log
+    char logContent[100];
+    snprintf(logContent, 100, "NewPeople: %s, Go: %d, Now: %d", people[peoCur-1].name, people[peoCur-1].targetFloor, people[peoCur-1].nowFloor);
+    printLog(logContent);
+}
+
 // Floor
 
 void printFloor(){
@@ -124,16 +180,25 @@ void printFloor(){
     }
 }
 
+//Logic
+
+void run(){
+    for(int i=0;i<50;i++){
+        generatePeople();
+        usleep(100000);
+    }
+}
+
 // Main
 
 int main(){
     printf("\033[?25l"); //hide cursor
     fflush(stdout);
+    srand(time(NULL)); // random seed
     initBlock();
     refreshElevator();
     printFloor();
-    elevatorGo(1);
-    elevatorGo(5);
+    run();
     screenPrintString(HEIGHT+1, 10, "\n");
     return 0;
 }
